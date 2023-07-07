b5787a6daf31ed26a2d5d32618cf075a063e323f
Christian <cmanalo.io@outlook.com>
2023-07-06 20:56:05 -0700

    update todo component

---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+
+const [edit, setEdit] = useState({
+    id:null,
+    value:''
+})
+ 
+function Todo() {
+  
+
+  return (
+    <div>Todo</div>
+  )
+}
+
+export default Todo
